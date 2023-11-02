1605d5053fcd6a524da210e0131974f627c505fd
Volodymyr Stelmakh <volodymyr.kai@gmail.com>
2023-11-02 22:01:26 +0200

    bug fixed

--- src/main.js
+++ src/main.js
@@ -7,3 +7,10 @@ document.querySelector(".header-btn").addEventListener('touchstart', () => {
 document.querySelector(".menu-btn").addEventListener('touchstart', () => {
     mob_menu.classList.remove('is-open');
 });
+const mob_backdrop = document.querySelector('.backdrop'); 
+document.querySelector(".header-btn").addEventListener('touchstart', () => {
+    mob_backdrop.classList.add('is-open');
+});
+document.querySelector(".menu-btn").addEventListener('touchstart', () => {
+    mob_backdrop.classList.remove('is-open');
+});
