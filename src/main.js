b41762dd91b3d031ea2fcc3f726123dd9f89077d
SerhiiChynin <sergey.chinin@gmail.com>
2023-11-03 14:29:59 +0200

    add js - mob_link_close

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 
 const mob_menu = document.querySelector('.menu'); 
+const close_item = document.querySelectorAll('.nav-item');
 
 document.querySelector(".header-btn").addEventListener('touchstart', () => {
     mob_menu.classList.add('is-open');
@@ -7,3 +8,11 @@ document.querySelector(".header-btn").addEventListener('touchstart', () => {
 document.querySelector(".menu-btn").addEventListener('touchstart', () => {
     mob_menu.classList.remove('is-open');
 });
+
+for (let i = 0; i < close_item.length; i++) {
+    close_item[i].addEventListener('touchstart', () => { 
+        mob_menu.classList.toggle('is-open');
+    })
+}
+
+
